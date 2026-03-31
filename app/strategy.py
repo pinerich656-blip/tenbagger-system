07c33e908baa1b7eaa75faafe41a90875c038347
pinerich656-blip <pinerich656@gmail.com>
2026-04-01 01:44:02 +0900

    Update strategy.py

--- app/strategy.py
+++ app/strategy.py
@@ -11,14 +11,7 @@ DEFAULT_STOCKS: list[StockInput] = [
     StockInput(name="ランディックス", code="2981.T"),
     StockInput(name="リアルゲイト", code="5532.T"),
     StockInput(name="ブロードエンタープライズ", code="4415.T"),
-    StockInput(name="パワーエックス", code="485A.T"),
     StockInput(name="ガーデン", code="274A.T"),
-    StockInput(name="マイクロアド", code="9553.T"),
-    StockInput(name="エッジテクノロジー", code="4268.T"),
-    StockInput(name="データX", code="3905.T"),
-    StockInput(name="ログリー", code="6579.T"),
-    StockInput(name="グッドパッチ", code="7351.T"),
-    StockInput(name="AI inside", code="4488.T"),
 ]
 
 
